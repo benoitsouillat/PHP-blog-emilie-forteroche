b4c7c6fc8e476f0861900883ebf5ab86322890ec
Benoit Souillat <dev.souillat@gmail.com>
2024-09-06 11:59:10 +0200

    Display of vues in admin

--- views/templates/admin.php
+++ views/templates/admin.php
@@ -1,20 +1,24 @@
-<?php 
-    /** 
-     * Affichage de la partie admin : liste des articles avec un bouton "modifier" pour chacun. 
-     * Et un formulaire pour ajouter un article. 
-     */
+<?php
+
+/** 
+ * Affichage de la partie admin : liste des articles avec un bouton "modifier" pour chacun. 
+ * Et un formulaire pour ajouter un article. 
+ */
 ?>
 
 <h2>Edition des articles</h2>
 
 <div class="adminArticle">
     <?php foreach ($articles as $article) { ?>
-        <div class="articleLine">
-            <div class="title"><?= $article->getTitle() ?></div>
-            <div class="content"><?= $article->getContent(200) ?></div>
-            <div><a class="submit" href="index.php?action=showUpdateArticleForm&id=<?= $article->getId() ?>">Modifier</a></div>
-            <div><a class="submit" href="index.php?action=deleteArticle&id=<?= $article->getId() ?>" <?= Utils::askConfirmation("Êtes-vous sûr de vouloir supprimer cet article ?") ?> >Supprimer</a></div>
+    <div class="articleLine">
+        <div class="title"><?= $article->getTitle() ?></div>
+        <div class="content"><?= $article->getContent(200) ?></div>
+        <div class="vues">Vues: <?= $article->getNumberVues() ?></div>
+        <div><a class="submit" href="index.php?action=showUpdateArticleForm&id=<?= $article->getId() ?>">Modifier</a>
         </div>
+        <div><a class="submit" href="index.php?action=deleteArticle&id=<?= $article->getId() ?>"
+                <?= Utils::askConfirmation("Êtes-vous sûr de vouloir supprimer cet article ?") ?>>Supprimer</a></div>
+    </div>
     <?php } ?>
 </div>
 
